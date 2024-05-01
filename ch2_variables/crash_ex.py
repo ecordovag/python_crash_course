dfc0e1621dedfc1ddc1a46e1a73b43115f1f5734
Eleonora <eleoc4@gmail.com>
2024-05-01 15:05:42 -0500

    Add the exercises and examples of ch. 2 - variables

---
+++ ch2_variables/crash_ex.py
@@ -0,0 +1,12 @@
+# f strings
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = "{first_name} {last_name}"
+print(full_name)
+
+import this
