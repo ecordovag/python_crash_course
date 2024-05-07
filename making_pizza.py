61daf260b7c8574b35b83a5ea94aec67cba9f636
Eleonora <eleoc4@gmail.com>
2024-05-06 23:24:25 -0500

    Add last changes

--- making_pizza.py
+++
@@ -1,7 +0,0 @@
-import pizza
-# To call a function from an imported module, enter the name of the module you 
-# imported, pizza, followed by the name of the function, 
-#make_pizza(), separated by a dot.
-
-pizza.make_pizza(16, 'pepperoni')
-pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
